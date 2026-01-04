5fd772972065882f0d2b834b094cd8976ffdfd86
hafidz1121 <hafidzfadhillah66@gmai.com>
2026-01-04 16:06:50 +0700

    update list_barang.php dari hafidz

--- views/list_barang.php
+++ views/list_barang.php
@@ -15,7 +15,7 @@
                 </li>
             </ul>
             <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Cari Data" aria-label="Search"/>
+                <input class="form-control me-2" type="search" placeholder="Cari Data ..." aria-label="Search"/>
                 <button class="btn btn-outline-success" type="submit">Cari</button>
             </form>
         </div>
